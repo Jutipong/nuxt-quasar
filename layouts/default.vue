8ba1c0a4db05c6dda57f167755f442742aa4e173
Jutipong.Dev <techcom.ec20@gmail.com>
2024-02-05 14:52:04 +0700

    Add menu list and isActive variable

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,47 @@ function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value
 }
 
+const isActive = ref('')
+
+const menuList = [
+  {
+    icon: 'inbox',
+    label: 'Inbox',
+    separator: true,
+  },
+  {
+    icon: 'send',
+    label: 'Outbox',
+    separator: false,
+  },
+  {
+    icon: 'delete',
+    label: 'Trash',
+    separator: false,
+  },
+  {
+    icon: 'error',
+    label: 'Spam',
+    separator: true,
+  },
+  {
+    icon: 'settings',
+    label: 'Settings',
+    separator: false,
+  },
+  {
+    icon: 'feedback',
+    label: 'Send Feedback',
+    separator: false,
+  },
+  {
+    icon: 'help',
+    iconColor: 'primary',
+    label: 'Help',
+    separator: false,
+  },
+]
+
 const icon = computed(() => leftDrawerOpen.value ? 'mdi-menu-open' : 'mdi-menu-close')
 
 function logout() {
@@ -35,7 +76,7 @@ function logout() {
               <q-avatar>
                 <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
               </q-avatar>
-              Title
+              Title : {{ isActive }}
             </q-toolbar-title>
 
             <q-space />
@@ -47,8 +88,29 @@ function logout() {
           </q-toolbar>
         </q-header>
 
-        <q-drawer v-model="leftDrawerOpen" show-if-above side="left" bordered>
-          <!-- drawer content -->
+        <!-- <q-drawer v-model="leftDrawerOpen" show-if-above side="left" bordered>
+        </q-drawer> -->
+        <q-drawer
+          v-model="leftDrawerOpen"
+          overlay
+          bordered
+          :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+        >
+          <q-scroll-area class="fit">
+            <q-list>
+              <template v-for="(menuItem, index) in menuList" :key="index">
+                <q-item v-ripple clickable :active="menuItem.label === 'Outbox'">
+                  <q-item-section avatar>
+                    <q-icon :name="`mdi-${menuItem.icon}`" />
+                  </q-item-section>
+                  <q-item-section>
+                    {{ menuItem.label }}
+                  </q-item-section>
+                </q-item>
+                <q-separator v-if="menuItem.separator" :key="`sep${index}`" />
+              </template>
+            </q-list>
+          </q-scroll-area>
         </q-drawer>
 
         <q-page-container>
